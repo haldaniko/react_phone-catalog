dbb8279139812676b90d7f50d75d13b5c2995c10
haldaniko <haldaniko@gmail.com>
2025-11-09 16:34:31 +0200

    add component 'Header'

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import * as React from 'react';
 import './App.scss';
+import { Header } from './components/Header/Header';
 
 export const App = () => (
   <div className="App">
-    <h1>Product Catalog</h1>
+    <Header />
   </div>
 );
