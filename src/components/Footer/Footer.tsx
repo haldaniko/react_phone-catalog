1467e56abc9da38d5b80975209a6c1163efb006e
haldaniko <haldaniko@gmail.com>
2025-11-09 16:47:48 +0200

    add 'Footer' component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './Footer.scss';
+
+export const Footer: React.FC = () => (
+  <footer className="footer">
+    <div className="footer__container">
+      <h1>Website Footer</h1>
+    </div>
+  </footer>
+);
