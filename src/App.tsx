1717e089c2c8e530c230c3322579b690eb4f1777
haldaniko <haldaniko@gmail.com>
2025-11-09 18:01:28 +0200

    reset 'body' styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import './App.scss';
 import { Header } from './components/Header/Header';
-import { Footer } from './components/Footer/Footer';
 
 export const App = () => (
   <div className="App">
     <Header />
-    <Footer />
   </div>
 );
