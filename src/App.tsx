a2cb9cc09232ce3affbddb4b189dfa2b9185d726
haldaniko <haldaniko@gmail.com>
2025-11-17 15:34:17 +0200

    implement Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import './App.scss';
 import { Header } from './components/Header/Header';
+import { Footer } from './components/Footer/Footer';
 
 export const App = () => (
   <div className="App">
     <Header />
+    <Footer />
   </div>
 );
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,8 +3,47 @@ import './Footer.scss';
 
 export const Footer: React.FC = () => (
   <footer className="footer">
-    <div className="footer__container">
-      <h1>Website Footer</h1>
+    <div className="footer__inner">
+      <div className="footer__brand">
+        <a href="#home" className="footer__logo" aria-label="Home">
+          <span className="footer__logo-text">
+            Nice
+            <img
+              className="footer__logo-image"
+              alt="Nice Gadgets Logo"
+              src="/logo.png"
+            />
+          </span>
+          <span className="footer__logo-text">Gadgets</span>
+        </a>
+      </div>
+
+      <div className="footer__nav">
+        <ul className="footer__nav-list">
+          <li className="footer__nav-item">
+            <a className="footer__nav-link" href="#">
+              Github
+            </a>
+          </li>
+          <li className="footer__nav-item">
+            <a className="footer__nav-link" href="#">
+              Contacts
+            </a>
+          </li>
+          <li className="footer__nav-item">
+            <a className="footer__nav-link" href="#">
+              Rights
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="footer__actions">
+        <p className="footer__action">Back to top</p>
+        <button className="footer__action-button">
+          <img src="/svg/Vector_forward.svg" alt="Heart" />
+        </button>
+      </div>
     </div>
   </footer>
 );
